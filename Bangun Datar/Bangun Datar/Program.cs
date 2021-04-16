2946476bf511a5a7339ff85a82597d01f64f308c
Alfahmist <48145002+alfahmist@users.noreply.github.com>
2021-04-16 09:10:09 +0700

    First Commit
    Bikin Project VS

---
+++ Bangun Datar/Bangun Datar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bangun_Datar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
